45e77b74e3ab5827e5089ee906fb89ed1d3ab7cd
Greenberg <irajgreenberg@gmail.com>
2024-03-20 09:19:53 +0000

    create demos and build steps for Node creative coding environment

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,9 @@
 const path = require("path");
 
 module.exports = {
-    // entry: "./src/p5/proj01/sketch.ts",
-    //entry: "./src/p5/CryptoIra/sketch.ts",
-    //entry: "./src/p5/CryptoIra02/sketch.ts",
-    //entry: "./src/p5/CryptoIra03/sketch.ts",
-    // entry: "./src/p5/CryptoIra04/sketch.ts",
-    //entry: "./src/p5/CryptoManDataCollector/sketch.ts",
-    entry: "./src/p5/VerletDemo_01/sketch.ts",
+    entry: "./src/p5/proj01/sketch.ts",
+    //entry: "./src/three/proj01/sketch.ts",
+    //entry: "./src/p5/VerletDemo_01/sketch.ts",
 
     module: {
         rules: [
